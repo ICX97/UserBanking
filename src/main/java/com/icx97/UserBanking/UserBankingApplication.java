e57d0369325c1465a33c4e56b729c60fb2463cda
ICX97 <igor.caran97@gmail.com>
2024-08-08 18:37:51 +0200

    Project init

---
+++ src/main/java/com/icx97/UserBanking/UserBankingApplication.java
@@ -0,0 +1,13 @@
+package com.icx97.UserBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icx97/UserBanking/UserBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icx97.UserBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
